592cb8281859f6babd5099f01f2383c49bb66ddf
Rapheal-Jones <45114972+Jones-2797GIT@users.noreply.github.com>
2020-11-14 08:08:07 +0530

    fourth

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -21,7 +21,7 @@ function App() {
     }
   }
   
-
+ 
   const dateBuilder = (d) => {
     
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
